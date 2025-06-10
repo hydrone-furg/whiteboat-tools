4bf665a71a86da95ca686b3793834bf42ece4104
Kauã <kauabarbosa1997@gmail.com>
2025-06-09 22:52:45 -0300

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 from common import MAVLinkConnection
 
-# mav = MAVLinkConnection(baud=57600)
-mav = MAVLinkConnection(sitl_address='udp:127.0.0.1:14550') # para usar via SITL
+# mav = MAVLinkConnection(baud=57600, simulating=False)
+mav = MAVLinkConnection(sitl_address='udp:127.0.0.1:14550', simulating=True) # para usar via SITL
 mav.connect()
 mav.read_sensors()
